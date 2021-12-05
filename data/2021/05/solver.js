15b28f96b71006f67ab9271dad96afee1cbe2597
seihoukei <a@zefiris.su>
2021-12-05 08:30:22 +0300

    Day 5

--- data/2021/05/solver.js
+++ data/2021/05/solver.js
@@ -1,12 +1,48 @@
-export function part1 (data, raw) {
-    let result = data.length
+class AocMap {
+    cells = []
 
-    return result
+    constructor(data, diagonal = false) {
+        this.diagonal = diagonal
+        for (let input of data) {
+            const [start, end] = input.split(" -> ").map(x => x.split(","))
+            this.draw(start, end)
+        }
+    }
+
+    draw(start, end) {
+        const dx = Math.sign(end[0] - start[0])
+        const dy = Math.sign(end[1] - start[1])
+
+        if (!this.diagonal && dx * dy)
+            return
+
+        const distance = Math.max(start[0] - end[0], end[0] - start[0], start[1] - end[1], end[1] - start[1])
+
+        for (let i = 0; i <= distance; i++) {
+            this.increase(+start[0] + dx * i, +start[1] + dy * i)
+        }
+
+    }
+
+    increase(x, y) {
+        const address = `${x},${y}`
+        this.cells[address] = (this.cells[address] ?? 0) + 1
+    }
+
+    get intersections() {
+        return Object.values(this.cells).reduce((v,x)=>x > 1 ? v+1 : v, 0)
+    }
+}
+
+export function part1 (data, raw) {
+    let map = new AocMap(data)
+
+    return map.intersections
 }
 
 export function part2 (data, raw) {
-    let result = raw.length
+    let map = new AocMap(data, true)
 
-    return result
+    return map.intersections
 }
 
