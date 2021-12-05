2f1e457ff3b0a03dbb40c41789ee9b64a538a92d
seihoukei <a@zefiris.su>
2021-12-05 18:39:50 +0300

    Day 5 cleanup

--- data/2021/05/solver.js
+++ data/2021/05/solver.js
@@ -1,5 +1,6 @@
 class AocMap {
-    cells = []
+    cells = new Set()
+    intersections = new Set()
 
     constructor(data, diagonal = false) {
         this.diagonal = diagonal
@@ -19,30 +20,33 @@ class AocMap {
         const distance = Math.max(start[0] - end[0], end[0] - start[0], start[1] - end[1], end[1] - start[1])
 
         for (let i = 0; i <= distance; i++) {
-            this.increase(+start[0] + dx * i, +start[1] + dy * i)
+            this.mark(+start[0] + dx * i, +start[1] + dy * i)
         }
 
     }
 
-    increase(x, y) {
-        const address = `${x},${y}`
-        this.cells[address] = (this.cells[address] ?? 0) + 1
+    mark(x, y) {
+        const cell = `${x},${y}`
+        if (this.cells.has(cell))
+            this.intersections.add(cell)
+        else
+            this.cells.add(cell)
     }
 
-    get intersections() {
-        return Object.values(this.cells).reduce((v,x)=>x > 1 ? v+1 : v, 0)
+    get result() {
+        return this.intersections.size
     }
 }
 
 export function part1 (data, raw) {
     let map = new AocMap(data)
 
-    return map.intersections
+    return map.result
 }
 
 export function part2 (data, raw) {
     let map = new AocMap(data, true)
 
-    return map.intersections
+    return map.result
 }
 
