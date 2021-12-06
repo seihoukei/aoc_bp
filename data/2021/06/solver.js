53c35c82c4c304272d6e58fd1dc24b9468eaacf5
seihoukei <a@zefiris.su>
2021-12-06 14:26:34 +0300

    Day 6

--- data/2021/06/solver.js
+++ data/2021/06/solver.js
@@ -1,12 +1,29 @@
-export function part1 (data, raw) {
-    let result = data.length
+class Tank {
+    fish = new Array(9).fill(0)
 
-    return result
+    constructor(data) {
+        for (let input of data)
+            this.fish[input]++
+    }
+
+    step() {
+        this.fish = this.fish.map((x,i,a) => i === 8 ? a[0] : i===6 ? a[7] + a[0] : a[i+1])
+    }
+
+    advance(times){
+        for (let i = 0; i < times; i++)
+            this.step()
+
+        return this.fish.reduce((v,x) => v + x)
+    }
+}
+export function part1 (data, raw) {
+    const tank = new Tank(raw.split(",").map(Number))
+    return tank.advance(80)
 }
 
 export function part2 (data, raw) {
-    let result = raw.length
-
-    return result
+    const tank = new Tank(raw.split(",").map(Number))
+    return tank.advance(256)
 }
 
