e0f80c85c50c9fe89d3dc8118f53db926a992985
seihoukei <a@zefiris.su>
2021-12-17 12:38:54 +0300

    Day 17

--- data/2021/17/solver.js
+++ data/2021/17/solver.js
@@ -1,18 +1,42 @@
 class Solver {
     constructor(data) {
-        for (let input of data) {
-
-        }
+        //target area: x=20..30, y=-10..-5
+        Object.assign(this, data[0].match(/x=(?<x0>[-0-9]+)\.\.(?<x1>[-0-9]+), y=(?<y0>[-0-9]+)\.\.(?<y1>[-0-9]+)/).groups)
     }
 
     get result() {
-        return 0
+        const ymax = -this.y0
+
+        return ymax * (ymax - 1) / 2
     }
 }
 
 class Solver2 extends Solver {
     get result() {
-        return 0
+        let count = 0
+        this.x0 = +this.x0
+        this.y0 = +this.y0
+        this.x1 = +this.x1
+        this.y1 = +this.y1
+        for (let dx = 1; dx <= this.x1; dx++)
+            for (let dy = this.y0; dy < -this.y0; dy++) {
+                let x = 0
+                let y = 0
+                let vx = dx
+                let vy = dy
+                while (y >= this.y0) {
+                    x += vx
+                    y += vy
+                    if (vx > 0)
+                        vx--
+                    vy -= 1
+                    if (x >= this.x0 && x <= this.x1 && y >= this.y0 && y <= this.y1) {
+                        count++
+                        break
+                    }
+                }
+            }
+        return count
     }
 }
 
