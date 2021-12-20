191c96dab52aae221fcef7dfb046c48e61828e87
seihoukei <a@zefiris.su>
2021-12-20 18:01:33 +0300

    Day 20 optimizations

--- data/2021/20/solver.js
+++ data/2021/20/solver.js
@@ -1,42 +1,45 @@
 class Solver {
     infinite = 0
+    map = []
     data = []
 
     constructor(data) {
         this.algorithm = [...data[0]].map(x => +(x==="#"))
-        this.map = data.slice(2).map(x => [...(x + "..")].map(x => +(x==="#")))
+        for (let input of data.slice(2)) {
+            let char = 0
+            const line = [...(input + "..")].map(x => char = char << 1 & 6 | +(x==="#"))
+            this.map.push(line)
+        }
     }
 
     step() {
         const empty = this.infinite * 7
+        const newChar = empty ^ 7
         this.infinite = this.algorithm[this.infinite * 511]
 
         const sx = this.map[0].length
-        const sy = this.map.length + 3
+        const sy = this.map.length + 2
 
-        for (let y = 0; y < this.map.length; y++) {
-            let line = this.data[y] ??= []
-            let char = empty
-            for (let x = 0; x < sx; x++)
-                line[x] = char = char << 1 & 6 | this.map[y][x]
-        }
+        ;[this.data, this.map] = [this.map, this.data]
 
         for (let y = 0; y < sy; y++) {
             let line = this.map[y] ??= []
+            let char = newChar
             for (let x = 0; x < sx; x++)
-                line[x] = this.algorithm[
+                line[x] = char = char << 1 & 6 | this.algorithm[
                     (this.data[y-2]?.[x] ?? empty) << 6 |
                     (this.data[y-1]?.[x] ?? empty) << 3 |
                     (this.data[y]?.[x] ?? empty)
                 ]
-            line[sx] = line[sx+1] = this.infinite
+            line[sx] = char = char << 1 & 6 | this.infinite
+            line[sx+1] = char = char << 1 & 6 | this.infinite
         }
     }
 
     enhance(steps) {
         for (let step = 0; step < steps; step++)
             this.step()
-        return this.map.reduce((v,x) => v + [...x].reduce((v,x) => v + x), 0)
+        return this.map.reduce((v,x) => v + x.reduce((v,x) => v + (x & 1)), 0)
     }
 }
 
