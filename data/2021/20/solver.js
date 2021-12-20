ab6548f7b978ef866e03e50fa6232ad44ccc32a9
seihoukei <a@zefiris.su>
2021-12-20 09:29:51 +0300

    Day 20

--- data/2021/20/solver.js
+++ data/2021/20/solver.js
@@ -1,59 +1,34 @@
 class Solver {
-    bits = new Set()
-    infinite = 0
+    infinite = "."
 
     constructor(data) {
         this.conversion = data[0]
-        let y = 0
-        this.x0 = -1
-        this.x1 = data[2].length
-        this.y0 = -1
-        for (let input of data.slice(2)) {
-            [...input].map((b,x) => b === "#" ? this.bits.add(`${x},${y}`) : 0)
-            y++
-        }
-        this.y1 = y
+        this.map = data.slice(2)
     }
 
     step() {
-        let nextBits = new Set()
-        for (let y = this.y0 - 1; y <= this.y1 + 1; y++){
-            for (let x = this.x0 - 1; x <= this.x1 + 1; x++) {
+        const newMap = []
+        for (let y = 0; y < this.map.length + 3; y++) {
+            let line = ""
+            for (let x = 0; x < this.map[0].length + 3; x++) {
                 let value = 0
-                for (let dy = -1; dy <= 1; dy++)
-                    for (let dx = -1; dx <= 1; dx++) {
+                for (let dy = 0; dy < 3; dy++)
+                    for (let dx = 0; dx < 3; dx++) {
                         value *= 2
-                        if (x + dx <= this.x0 || x + dx >= this.x1 || y + dy <= this.y0 || y + dy >= this.y1)
-                            value += this.infinite
-                        else
-                            value += this.bits.has(`${x + dx},${y + dy}`) ? 1 : 0
+                        value += (this.map[y+dy-2]?.[x+dx-2] ?? this.infinite) === "#" ? 1 : 0
                     }
-                    if (this.conversion[value] === "#")
-                        nextBits.add(`${x},${y}`)
+                line += this.conversion[value]
             }
+            newMap.push(line)
         }
-        this.infinite = (this.conversion[this.infinite ? 511 : 0] === "#") ? 1 : 0
-        this.x0 -= 1
-        this.y0 -= 1
-        this.x1 += 1
-        this.y1 += 1
-        this.bits = nextBits
-    }
-
-    log() {
-        for (let y = this.y0; y <= this.y1; y++) {
-            let s = ""
-            for (let x = this.x0; x <= this.x1; x++) {
-                s += this.bits.has(`${x},${y}`) ? "#" : "."
-            }
-            console.log(s)
-        }
+        this.map = newMap
+        this.infinite = this.conversion[this.infinite === "." ? 0 : 511]
     }
 
     result(steps) {
         for (let step = 0; step < steps; step++)
             this.step()
-        return this.bits.size
+        return this.map.reduce((v,x) => v + [...x].reduce((v,x) => v + (x === "#" ? 1 : 0),0), 0)
     }
 }
 
