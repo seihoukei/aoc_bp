dca114c299044b8c7dbac4eea7e28e83e1d90b6a
seihoukei <a@zefiris.su>
2021-12-20 17:44:32 +0300

    Day 20 optimizations

--- data/2021/20/solver.js
+++ data/2021/20/solver.js
@@ -1,34 +1,42 @@
 class Solver {
-    infinite = "."
+    infinite = 0
+    data = []
 
     constructor(data) {
-        this.conversion = data[0]
-        this.map = data.slice(2)
+        this.algorithm = [...data[0]].map(x => +(x==="#"))
+        this.map = data.slice(2).map(x => [...(x + "..")].map(x => +(x==="#")))
     }
 
     step() {
-        const newMap = []
-        for (let y = 0; y < this.map.length + 3; y++) {
-            let line = ""
-            for (let x = 0; x < this.map[0].length + 3; x++) {
-                let value = 0
-                for (let dy = 0; dy < 3; dy++)
-                    for (let dx = 0; dx < 3; dx++) {
-                        value *= 2
-                        value += (this.map[y+dy-2]?.[x+dx-2] ?? this.infinite) === "#" ? 1 : 0
-                    }
-                line += this.conversion[value]
-            }
-            newMap.push(line)
+        const empty = this.infinite * 7
+        this.infinite = this.algorithm[this.infinite * 511]
+
+        const sx = this.map[0].length
+        const sy = this.map.length + 3
+
+        for (let y = 0; y < this.map.length; y++) {
+            let line = this.data[y] ??= []
+            let char = empty
+            for (let x = 0; x < sx; x++)
+                line[x] = char = char << 1 & 6 | this.map[y][x]
         }
-        this.map = newMap
-        this.infinite = this.conversion[this.infinite === "." ? 0 : 511]
+
+        for (let y = 0; y < sy; y++) {
+            let line = this.map[y] ??= []
+            for (let x = 0; x < sx; x++)
+                line[x] = this.algorithm[
+                    (this.data[y-2]?.[x] ?? empty) << 6 |
+                    (this.data[y-1]?.[x] ?? empty) << 3 |
+                    (this.data[y]?.[x] ?? empty)
+                ]
+            line[sx] = line[sx+1] = this.infinite
+        }
     }
 
-    result(steps) {
+    enhance(steps) {
         for (let step = 0; step < steps; step++)
             this.step()
-        return this.map.reduce((v,x) => v + [...x].reduce((v,x) => v + (x === "#" ? 1 : 0),0), 0)
+        return this.map.reduce((v,x) => v + [...x].reduce((v,x) => v + x), 0)
     }
 }
 
@@ -36,13 +44,12 @@ export function part1 (data, raw) {
 //    let solver = new Solver(raw.split(",").map(Number))
     let solver = new Solver(data)
 
-    return solver.result(2)
+    return solver.enhance(2)
 }
 
 export function part2 (data, raw) {
-//    let solver = new Solver2(raw.split(",").map(Number))
     let solver = new Solver(data)
 
-    return solver.result(50)
+    return solver.enhance(50)
 }
 
